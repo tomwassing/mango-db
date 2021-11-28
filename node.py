2c5386d9a88202b521f4ee41d69cd5c3a4aef683
Tom J. Wassing <tjwassing@gmail.com>
2021-11-28 16:46:55 +0100

    Removed ZeroMQ

--- node.py
+++ node.py
@@ -1,11 +1,9 @@
-import sys
-import zmq
 import threading
 import logging
-import time
 import json
 import random
 import signal
+import socket
 
 '''
 TODO: 
@@ -14,83 +12,57 @@ TODO:
     - Ordering of messages
 '''
 
+BUFFER_SIZE = 1024
+
 class Node:
-    def __init__(self, port, node_ports):
+    def __init__(self, port, ports):
         self.port = port
-        self.sockets = []
+        self.ports = ports
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("", port))
 
-        context = zmq.Context()
-
-        # Start listening for messages from other nodes.
-        socket = context.socket(zmq.PAIR)
-        logging.info(f"Node:{port} binding on port {self.port}...")
-        socket.bind(f"tcp://*:{port}")
-        self.sockets.append((port, socket))
-
-        # Connecting to all other nodes.
-        for node_port in node_ports:
-            socket = context.socket(zmq.PAIR) 
-            logging.info(f"Node:{port} connecting to port {node_port}...")
-            socket.connect(f"tcp://localhost:{node_port}")
-            self.sockets.append((node_port, socket))
+        logging.info(f"Node:{port} listining on port {self.port}")
 
     def run(self):
-        # Setup polling on all sockets.
-        poller = zmq.Poller()
-        for _, socket in self.sockets:
-            poller.register(socket, zmq.POLLIN)
-
-        logging.info(f"Node:{self.port} started on port {self.port}")
         while True:
-            socks = dict(poller.poll(100))
-            for sock_port, sock in self.sockets:
-                if socks.get(sock) == zmq.POLLIN:
-                    result = sock.recv_string()
-                    self.on_message(sock_port, sock, json.loads(result))
-                    logging.debug(f"Node:{self.port}, received message: {result} from node:{sock_port}")
+            data, addr = self.socket.recvfrom(BUFFER_SIZE)
+            message = json.loads(data.decode())
+            logging.debug(f"Node:{self.port}, received message: {message} from {addr}")
+            self.on_message(addr, message)
 
     def send_to_all(self, data):
-        message = json.dumps(data)
-        logging.info(f"Node:{self.port} sending message: {message}")
-        for node_port, socket in self.sockets:
-            if node_port != self.port:
-                logging.debug(f"Node:{self.port} send message to node:{node_port}")
-                socket.send_string(message)
+        for port in self.ports:
+            self.send(("127.0.0.1", port), data)
 
-    def send(self, socket, message):
-        socket.send_string(json.dumps(message))
+    def send(self, addr, message):
+        self.socket.sendto(json.dumps(message).encode(), addr)
+        logging.debug(f"Node:{self.port}, sent message: {message} to {addr}")
 
-    def on_message(self, port, socket, data):
+    def on_message(self, addr, message):
         pass
 
 
 class PendingElement:
-    def __init__(self, key, value, msg_id, client_socket):
+    def __init__(self, key, value, msg_id, client_addr):
         self.key = key
         self.value = value
         self.msg_id = msg_id
         self.acknowledged = set()
-        self.client_socket = client_socket
+        self.client_addr = client_addr
     
     def acknowledge(self, node_port):
         self.acknowledged.add(node_port)
 
-    def is_complete(self, sockets):
-        return len(self.acknowledged) == len(sockets) - 1
+    def is_complete(self, number_of_nodes):
+        return len(self.acknowledged) == number_of_nodes
 
 
 class Client:
     def __init__(self, node_ports):
-        self.sockets = []
+        self.node_ports = node_ports
 
-        context = zmq.Context()
-
-        # Connecting to all other nodes.
-        for node_port in node_ports:
-            socket = context.socket(zmq.PAIR) 
-            logging.info(f"Client connecting to port {node_port}...")
-            socket.connect(f"tcp://localhost:{node_port}")
-            self.sockets.append((node_port, socket))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("", 0))
 
     def write(self, key, value):
         data = {
@@ -99,11 +71,11 @@ class Client:
             "value": value
         }
 
-        _, socket = random.choice(self.sockets)
-        socket.send_string(json.dumps(data))
-        logging.info(f"Client: send message: {data}")
-        result = socket.recv_string()
-        logging.info(f"Client: received message: {result}")
+        port = random.choice(self.node_ports)
+        self.socket.sendto(json.dumps(data).encode(), ("127.0.0.1", port))
+        logging.info(f"Client: send message to node:{port} : {data}")
+        data, addr = self.socket.recvfrom(BUFFER_SIZE)
+        logging.info(f"Client: received message: {data} from {addr}")
 
 
 class Follower(Node):
@@ -113,69 +85,73 @@ class Follower(Node):
         self.write_buffer = {}
         self.write_id = 0
 
-    def handle_acknowledgement(self, socket, msg_id):
+    def handle_acknowledgement(self, addr, msg_id):
         data = {
             "type": "acknowledge",
-            "id": msg_id
+            "id": msg_id,
+            "from": self.port,
         }
 
-        self.send(socket, data)
+        self.send(addr, data)
 
-    def write(self, key, value, socket):
+    def write(self, key, value, addr):
         '''Add key-value pair to acknowledge buffer and send write message to
         all the other nodes.'''
         msg_id = f"{self.port}:{self.write_id}"
-        self.ack_buffer[msg_id] = PendingElement(key, value, msg_id, socket)
+        self.ack_buffer[msg_id] = PendingElement(key, value, msg_id, addr)
         self.write_id += 1
 
         data = {
             "type": "write",
             "id": msg_id,
             "key": key,
-            "value": value
+            "value": value,
+            "from": self.port,
         }
         
         self.send_to_all(data)
 
-    def on_message(self, port, socket, data):
+    def on_message(self, addr, data):
         if data["type"] == "client_write":
             logging.debug(f"Follower:{self.port}: received client_write message: {data} from client")
-            self.write(data["key"], data["value"], socket)
+            self.write(data["key"], data["value"], addr)
 
         elif data["type"] == "write":
             # Handling incoming write message from other nodes. Ack the message and
             # add to own write buffer.
-            logging.debug(f"Follower:{self.port}: received write message: {data} from node:{port}")
+            logging.debug(f"Follower:{self.port}: received write message: {data} from node:{addr}")
             self.write_buffer[data["id"]] = (data["key"], data["value"])
-            self.handle_acknowledgement(socket, data["id"])
+            self.handle_acknowledgement(addr, data["id"])
         
         elif data["type"] == "acknowledge":
             # Receiving ack message from other nodes, finalize if all ack messages
             # have been received
             msg_id = data["id"]
-            logging.debug(f"Follower:{self.port}: received acknowledge message: {data} from node:{port}")
-            self.ack_buffer[msg_id].acknowledge(socket)
+            logging.debug(f"Follower:{self.port}: received acknowledge message: {data} from node:{addr}")
+            self.ack_buffer[msg_id].acknowledge(addr)
 
-            if self.ack_buffer[msg_id].is_complete(self.sockets):
+            if self.ack_buffer[msg_id].is_complete(len(self.ports)):
                 logging.debug(f"Follower:{self.port}: received all acknowledgements for message: {msg_id}")
                 self.write_buffer[msg_id] = self.ack_buffer[msg_id]
                 del self.ack_buffer[msg_id]
-                logging.debug(f"Follower:{self.port}: sent write_result message: {self.write_buffer[msg_id]} to client")
 
-                self.write_buffer[msg_id].client_socket.send_string(json.dumps({
+                receiver = self.write_buffer[msg_id].client_addr
+                data = {
                     "type": "write_result",
                     "key": self.write_buffer[msg_id].key,
                     "value": self.write_buffer[msg_id].value
-                }))
+                }
+
+                self.send(receiver, data)
 
 
-def main(num_nodes, start_port=5000):
+def main(num_nodes, start_port=25000):
     logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s',
                         level=logging.DEBUG,datefmt='%d-%m-%y %H:%M:%S')
 
     node_ports = list(range(start_port, start_port + num_nodes))
     nodes = [Follower(port, [p for p in node_ports if p != port]) for port in node_ports]
-    threads = [threading.Thread(target=node.run, daemon=True) for node in nodes]
+    threads = [threading.Thread(target=node.run) for node in nodes]
 
     for thread in threads:
         thread.start()
@@ -186,4 +162,4 @@ def main(num_nodes, start_port=5000):
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    main(3)
+    main(4)
