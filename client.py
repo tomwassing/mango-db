5de7d92dc89e9571abb77fdc512acfd89a6455f3
jurre brandsen <Jurrebrandsen@gmail.com>
2021-12-03 17:48:16 +0100

    Added additional basic test and started working on the durability tests

--- client.py
+++ client.py
@@ -10,9 +10,12 @@ class Client:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(("", 0))
+        self.socket.settimeout(.5)
 
-    def send_recv(self, data):
-        port = random.choice(self.node_ports)
+    # Sync operation
+    def send_recv(self, data, port=None):
+        if not port:
+            port = random.choice(self.node_ports)
         self.socket.sendto(json.dumps(data).encode(), ("127.0.0.1", port))
         logging.info(f"Client: send message to node:{port} : {data}")
         data, addr = self.socket.recvfrom(1024)
@@ -23,22 +26,35 @@ class Client:
         for port in self.node_ports:
             self.socket.sendto(json.dumps(data).encode(), ("127.0.0.1", port))
 
-    def write(self, key, value):
+    def write(self, key, value, port=None, blocking=True):
         data = {
             "type": "client_write",
             "key": key,
             "value": value
         }
 
-        self.send_recv(data)
+        if blocking:
+            self.send_recv(data, port=port)
+        else:
+            self.socket.sendto(json.dumps(data).encode(), ("127.0.0.1", port))
 
-    def read(self, key):
+    def write_recv(self, key, value, port=None):
+        try:
+            data, addr = self.socket.recvfrom(1024)
+            logging.info(f"Client: received message: {data} from {addr}")
+            return json.loads(data.decode())
+        except socket.timeout:
+            # Follower has been disconnected. Resend request to different node.
+            logging.info(f"Client: received timeout from {port}")
+            self.write(key, value, port=random.choice([x for x in self.node_ports if x != port]), blocking=False)
+
+    def read(self, key, port=None):
         data = {
             "type": "client_read",
             "key": key,
         }
 
-        return self.send_recv(data)
+        return self.send_recv(data, port=port)
 
     def exit(self):
         data = {
@@ -47,3 +63,10 @@ class Client:
 
         self.send_all(data)
         self.socket.close()
+
+    def exit_single(self, port):
+        data = {
+            "type": "exit"
+        }
+
+        self.socket.sendto(json.dumps(data).encode(), ("127.0.0.1", port))
--- test_basic.py
+++ test_basic.py
@@ -2,44 +2,99 @@ import threading
 from follower import Follower
 from leader import Leader
 from client import Client
+import random
 import multiprocessing
+import pytest
+from unittest.mock import Mock
 
-def setup(num_nodes, start_port=25000):
-    node_ports = list(range(start_port, start_port + num_nodes))
+def setup(num_nodes, num_clients, start_port=25000):
+    node_ports = list(range(start_port, start_port + num_nodes + num_clients))
     nodes = [Follower(port, [p for p in node_ports if p != port], node_ports[-1]) for port in node_ports[:-1]]
     leader = Leader(node_ports[-1], node_ports[:-1], node_ports[-1])
     processes = [threading.Thread(target=node.run) for node in [leader, *nodes]]
-    client = Client(node_ports)
-    
+    clients = [Client(node_ports) for _ in range(num_clients)]
+
     for process in processes:
         process.start()
 
-    return nodes, leader, client, processes
+    return node_ports, nodes, leader, clients, processes
 
 class TestBasic:
 
     def setup_method(self, method):
-        nodes, leader, client, processes = setup(3)
+        _, nodes, leader, clients, processes = setup(3, 5)
         self.nodes = nodes
         self.leader = leader
-        self.client = client
+        self.clients = clients
         self.processes = processes
 
-    def teardown_method(self, method):
-        self.client.exit()
+    def teardown_method(self):
+        for client in self.clients:
+            client.exit()
         for process in self.processes:
             process.join()
 
     def test_read_after_write(self):
-        self.client.write("World!", 'Hello?')
+        client = self.clients[0]
+        client.write("World!", 'Hello?')
 
-        assert self.client.read('World!')["value"] == 'Hello?'
+        assert client.read('World!')["value"] == 'Hello?'
 
     def test_read_after_five_writes(self):
-        self.client.write("World!", 'Hello1?')
-        self.client.write("World!", 'Hello2?')
-        self.client.write("World!", 'Hello3?')
-        self.client.write("World!", 'Hello4?')
-        self.client.write("World!", 'Hello5?')
+        client = self.clients[0]
+        client.write("World!", 'Hello1?')
+        client.write("World!", 'Hello2?')
+        client.write("World!", 'Hello3?')
+        client.write("World!", 'Hello4?')
+        client.write("World!", 'Hello5?')
 
-        assert self.client.read('World!')["value"] == 'Hello5?'
+        assert client.read('World!')["value"] == 'Hello5?'
+
+    def test_read_on_all_clients(self):
+        write_client = random.choice(self.clients)
+        write_client.write("World!", 'Hello')
+        for client in self.clients:
+            assert client.read('World!')["value"] == 'Hello'
+
+    def test_write_read_different_client(self):
+        write_client, read_client = random.sample(self.clients, 2)
+        write_client.write("World!", 'Hello')
+        assert read_client.read('World!')["value"] == 'Hello'
+
+class TestDurability:
+    def setup_method(self, method):
+        node_ports, nodes, leader, clients, processes = setup(5, 1)
+        self.node_ports = node_ports
+        self.nodes = nodes
+        self.leader = leader
+        self.clients = clients
+        self.processes = processes
+
+    def teardown_method(self):
+        for client in self.clients:
+            client.exit()
+        for process in self.processes:
+            process.join()
+
+    def test_connection_lost_write(self):
+        client = self.clients[0]
+        node_number = random.randint(0, len(self.nodes) - 1)
+        error_node = self.nodes[node_number]
+
+        print(error_node.port)
+
+        client.write("World!", 'Hello?', port=error_node.port, blocking=False)
+        client.exit_single(error_node.port)
+
+        # Wait till thread is done.
+        self.processes[node_number + 1].join()
+
+        # after timeout, client sends to different follower.
+        client.write_recv("World!", 'Hello?', port=error_node.port)
+        new_port = random.choice([x for x in self.node_ports if x != error_node.port])
+        print(new_port)
+        assert client.read('World!', port=new_port)["value"] == 'Hello?'
+
+        # assert client.read('World!')["value"] == 'Hello?'
+
+
