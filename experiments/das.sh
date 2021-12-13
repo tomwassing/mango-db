4e612a0193644bc44c476b28d2f5004e6ddabefc
Tom J. Wassing <tjwassing@gmail.com>
2021-12-13 12:15:00 +0100

    Fixed arrays

--- experiments/das.sh
+++ experiments/das.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # cores
-for n in [4]; do
+for n in 4; do
 
     # runs
     for i in {1..10}; do
 
         # read_heavy or write_heavy
-        for j in [0, 1]; do
+        for j in 0 1; do
 
             # order_on_write or order_on_read 
-            for k in [0, 1]; do
+            for k in 0 1; do
                 echo "run $i, read_heavy $j, order_on_write $k, nodes $n"
                 prun -v -1 -np $n python3 ./perf_exp_das.py $j $k
                 sleep 1
