5c648c40fc01dc42fb81861a4f72ad4688abb88f
Tom J. Wassing <tjwassing@gmail.com>
2021-12-14 21:02:29 +0100

    Fixed script

--- experiments/das_2_sync.sh
+++ experiments/das_2_sync.sh
@@ -12,7 +12,7 @@ for n in 5 9 3; do
             # order_on_write or order_on_read 
             for k in 0 1; do
                 echo "run $i, read_heavy $j, order_on_write $k, nodes $n"
-                prun -v -1 -np $n python3 ./perf_exp_2_das.py $j $k
+                prun -v -1 -np $n python3 ./perf_exp_2_das_sync.py $j $k
                 sleep 1
             done
         done
