4941732e7bfd9a2f42b65d8d62ad6b9d628d61b1
Tom J. Wassing <tjwassing@gmail.com>
2021-12-14 22:26:49 +0100

    Added experiment 3

--- experiments/perf_exp_3_das.py
+++ experiments/perf_exp_3_das.py
@@ -10,41 +10,36 @@ from experiment import Experiment
 from system import DasSystem
 
 
-import perf_exp_2
+import perf_exp_1
 
-def main(order_on_write, read_heavy):
+def main(order_on_write, num_clients):
     logging.basicConfig(format='%(asctime)s.%(msecs)03d - %(levelname)-8s: %(message)s',
                         level=logging.WARNING,datefmt='%d-%m-%y %H:%M:%S')
     
     hostnames = os.getenv('HOSTS').split()
     hostname = socket.gethostname()
 
-    system = DasSystem(num_clients=10, port=25000, order_on_write=order_on_write)
+    system = DasSystem(num_clients=num_clients, port=25000, order_on_write=order_on_write)
     if hostname != hostnames[0]:
         system.start()
         return
 
     time.sleep(15)
 
-    read_factor = 2 if read_heavy else 1
-    write_factor = 1 if read_heavy else 2
-
     experiment = Experiment(
-        experiment_name='Performance Experiment 2',
+        experiment_name='Performance Experiment 3',
         systems=[system],
-        n_writes=100000 * write_factor,
-        n_reads=100000 * read_factor,
+        n_writes=100000,
+        n_reads=100000,
     )
 
     # Run experiment 5 times
     print("{}".format(experiment.__str__()))
     start = time.perf_counter()
 
-    exp_func = perf_exp_2.read_heave_exp_func if read_heavy else perf_exp_2.write_heavy_exp_func
-
-    results = list(experiment.run_multi_client(exp_func, repeat=1))[0]
+    results = list(experiment.run_multi_client(perf_exp_1.experiment_func, repeat=1))
     columns = ["system_name", "run_id", "latency", "operation", "on_leader", "n_nodes", "n_clients", "order_on_write"]
-    filename = "./results/experiment2_{}_{}_{}.csv".format(order_on_write, read_heavy, datetime.today().strftime("%Y%m%d%H%M%S"))
+    filename = "./results/experiment3_{}_{}.csv".format(order_on_write, datetime.today().strftime("%Y%m%d%H%M%S"))
 
     with open(filename, 'w', encoding="utf-8") as f:
         writer = csv.writer(f)
@@ -56,5 +51,5 @@ def main(order_on_write, read_heavy):
 
 if __name__ == '__main__':
     order_on_write = len(sys.argv) > 1 and int(sys.argv[1]) == 1
-    read_heavy = len(sys.argv) > 2 and int(sys.argv[2]) == 1
-    main(order_on_write, read_heavy)
+    num_clients = int(sys.argv[2])
+    main(order_on_write, num_clients)
