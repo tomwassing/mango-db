123b7adc2b97e173ceeb2f33f3f0a2e7f80b354b
Tom J. Wassing <tjwassing@gmail.com>
2021-12-14 22:33:03 +0100

    Fixed command line ordering

--- experiments/das_3.sh
+++ experiments/das_3.sh
@@ -12,7 +12,7 @@ for n in 5 9 3; do
             # order_on_write or order_on_read 
             for k in 0 1; do
                 echo "run $i, clients $j, order_on_write $k, nodes $n"
-                prun -v -1 -np $n python3 ./perf_exp_3_das.py $j $k
+                prun -v -1 -np $n python3 ./perf_exp_3_das.py $k $j
                 sleep 1
             done
         done
